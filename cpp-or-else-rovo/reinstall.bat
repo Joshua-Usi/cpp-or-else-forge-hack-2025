2f4b7ba189e81dd6be7911e7c7760d83cd8c71ca
Joshua Usi <joshua.usi2017@gmail.com>
2025-02-11 10:55:14 +1100

    Pen

--- cpp-or-else-rovo/reinstall.bat
+++ cpp-or-else-rovo/reinstall.bat
@@ -1,8 +1,7 @@
 @echo off
 call set_env.bat
 @echo on
-call forge uninstall
 call forge deploy
-:: We need to install on both platforms
-call forge install --confirm-scopes -s atl-forgehack2-team-11.atlassian.net -p Confluence
-call forge install --confirm-scopes -s atl-forgehack2-team-11.atlassian.net -p Jira
+:: We need to install on both platforms 
+call forge install --upgrade --confirm-scopes -s atl-forgehack2-team-11.atlassian.net -p Confluence
+call forge install --upgrade --confirm-scopes -s atl-forgehack2-team-11.atlassian.net -p Jira
