df9eb5d908225675a4bb709f4ad6b51e2ff7eaef
shuumo <arnav.divesh.singh@gmail.com>
2025-02-10 20:44:00 +1100

    fixed formatting thats all

--- cpp-or-else-rovo/src/index.js
+++ cpp-or-else-rovo/src/index.js
@@ -5,29 +5,29 @@ export function messageLogger(payload) {
 }
 
 export async function fetchFile(payload) {
-  const title = payload.message
-  console.log(JSON.stringify(payload, null, 2))
-  console.log("hello")
-
-	const response = await api.asUser().requestConfluence(route`/wiki/api/v2/pages?title=${title}`, {
-  headers: {
-    'Accept': 'application/json'
-  }
-});
-
-const data = await response.json()
-
-
-const result = await Promise.all(
-  data.results.map(async (page) => {
-    const pageResponse = await api.asUser().requestConfluence(route`/wiki/api/v2/pages/${page.id}?body-format=atlas_doc_format`, {
-      headers: {
-        'Accept': 'application/json'  // Fix typo here
-      }
+    const title = payload.message
+    console.log(JSON.stringify(payload, null, 2))
+    console.log("hello")
+    
+    const response = await api.asUser().requestConfluence(route`/wiki/api/v2/pages?title=${title}`, {
+        headers: {
+            'Accept': 'application/json'
+        }
     });
-    return pageResponse.json();  // Await JSON parsing
-  })
-);
+    
+    const data = await response.json()
 
-  return result
+
+    const result = await Promise.all(
+        data.results.map(async (page) => {
+            const pageResponse = await api.asUser().requestConfluence(route`/wiki/api/v2/pages/${page.id}?body-format=atlas_doc_format`, {
+            headers: {
+                'Accept': 'application/json'  // Fix typo here
+            }
+            });
+            return pageResponse.json();  // Await JSON parsing
+        })
+        );
+    
+        return result
 }
